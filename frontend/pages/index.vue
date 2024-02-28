405f6345a3d4bf5190b9ec416993d0ac0e70aaa9
shimi-lynx <n33etc@gmail.com>
2024-02-28 21:51:40 +0900

    Set up Nuxt 3 project

---
+++ frontend/pages/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <h1>Welcome to the pages/index.vue</h1>
+    <AppAlert>
+      This is an auto-imported component
+    </AppAlert>
+    <li><NuxtLink to="/about">/about</NuxtLink></li>
+    <li><NuxtLink to="/hello/world">/slug</NuxtLink></li>
+    <li><NuxtLink to="/parent">/parent</NuxtLink></li>
+  </div>
+</template>
